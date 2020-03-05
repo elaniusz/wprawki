4a9376756a1fe96124a67ac67d8e8f4202059533
Marek Michalik <marekmichalik@Mac-mini-Marek.local>
2020-03-05 20:16:51 +0100

    cykle zycia

--- src/app/component/person-detail/person-detail.component.ts
+++ src/app/component/person-detail/person-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
 import { Advenced, Person } from 'src/app/model';
 
 @Component({
@@ -6,20 +6,38 @@ import { Advenced, Person } from 'src/app/model';
   templateUrl: './person-detail.component.html',
   styleUrls: ['./person-detail.component.scss']
 })
-export class PersonDetailComponent implements OnInit {
-
+export class PersonDetailComponent implements OnInit, OnDestroy, OnChanges {
+  
   @Input() person: Person;
   @Output() shift = new EventEmitter<string>();
 
   nameColor = '#009999';
   isActive = false;
   showPhoto = false;
+  licznik = 0;
+  licznikHandle: number;
 
   Advenced = Advenced;
 
   constructor() { }
 
   ngOnInit() {
+    console.log('inicjalizacja komponentu');
+    //this.licznikHandle = setInterval(() => {
+      //this.licznik++;
+     // console.log('licznik++');
+    //}, 1000 );
+  }
+
+  ngOnDestroy(): void {
+    console.log('zniszczenie komponentu');
+    //clearInterval(this.licznikHandle);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!changes.person.firstChange) {
+    console.log(`zmiana z ${changes.person.previousValue.name} na ${changes.person.currentValue.name}`);
+    }
   }
 
   zmienKolor() {
